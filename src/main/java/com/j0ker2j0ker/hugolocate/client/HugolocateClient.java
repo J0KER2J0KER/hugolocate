bdc8a7a22a8391392cf130ad042dcce92db834a3
J0KER2J0KER <furs-weekday1q@icloud.com>
2026-07-05 09:47:13 +0200

    Initial Commit
    Signed-off-by: J0KER2J0KER <furs-weekday1q@icloud.com>

---
+++ src/main/java/com/j0ker2j0ker/hugolocate/client/HugolocateClient.java
@@ -0,0 +1,10 @@
+package com.j0ker2j0ker.hugolocate.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HugolocateClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
