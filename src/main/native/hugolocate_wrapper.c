081a2659d5ab417892bb9eb66e9de632adca485f
J0KER2J0KER <furs-weekday1q@icloud.com>
2026-07-09 08:58:44 +0200

    alle dimensionen von cubiomes können jetzt genutzt werden
    Signed-off-by: J0KER2J0KER <furs-weekday1q@icloud.com>

--- src/main/native/hugolocate_wrapper.c
+++ src/main/native/hugolocate_wrapper.c
@@ -9,14 +9,15 @@
 
 /*
  * Returns 1 if the given structure type is biome-viable at (x, z) for the
- * given Minecraft version and seed, 0 otherwise.
+ * given Minecraft version, seed and dimension, 0 otherwise.
  *
  * mc:   Minecraft version constant (see cubiomes generator.h, e.g. MC_1_21)
  * structureType: cubiomes structure enum value (see finders.h, e.g. Shipwreck)
+ * dim:  dimension constant (see cubiomes generator.h: DIM_OVERWORLD, DIM_NETHER, DIM_END)
  */
-int hugolocate_is_viable(int mc, int structureType, long long seed, int x, int z) {
+int hugolocate_is_viable(int mc, int structureType, long long seed, int x, int z, int dim) {
     Generator g;
     setupGenerator(&g, mc, 0);
-    applySeed(&g, DIM_OVERWORLD, (uint64_t) seed);
+    applySeed(&g, dim, (uint64_t) seed);
     return isViableStructurePos(structureType, &g, x, z, 0);
 }
